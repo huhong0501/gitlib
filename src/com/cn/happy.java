82d45b6cdeb558c7ecff910a9bb5288cb7ec750b
huhong <965761737@qq.com>
2020-01-02 14:36:06 +0800

    1

---
+++ src/com/cn/happy.java
@@ -0,0 +1,13 @@
+package com.cn;
+
+public class happy {
+    public void heihei(){
+
+    }
+
+
+    public static void main(String[] args) {
+        happy h = new happy();
+        h.heihei();
+    }
+}
