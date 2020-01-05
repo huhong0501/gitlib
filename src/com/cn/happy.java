468f8d2383dfe200f7d285fe1aaa79424bf0695f
huhong <965761737@qq.com>
2020-01-05 18:25:45 +0800

    getset方法

--- src/com/cn/happy.java
+++ src/com/cn/happy.java
@@ -1,13 +1,25 @@
 package com.cn;
 
 public class happy {
-    public void heihei(){
+   public int age;
 
+    public int getAge() {
+        System.out.println("年龄"+age);
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public happy(String name){
+        System.out.println("名字是"+name);
     }
 
 
     public static void main(String[] args) {
-        happy h = new happy();
-
+        happy h = new happy("狗");
+      h.setAge(8);
+      h.getAge();
     }
 }
